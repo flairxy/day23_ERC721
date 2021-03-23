1a705bb0ebeb487f00faec1d683d38afcd26153c
flairxy <okonkwo.miracle@raadaa.com>
2021-03-23 10:54:49 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Drizzle } from "@drizzle/store";
+import { drizzleReactHooks } from "@drizzle/react-plugin";
+import drizzleOptions from "./drizzleOptions";
+import LoadingComponent from "./loadingComponent";
+import TokenMetadata from "./TokenMetadata";
+import TokenWallet from "./TokenWallet";
+
+const drizzle = new Drizzle(drizzleOptions);
+const { DrizzleProvider } = drizzleReactHooks;
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>ERC20 Token</h1>
+      <DrizzleProvider drizzle={drizzle}>
+        <LoadingComponent>
+          <TokenMetadata />
+          <TokenWallet />
+        </LoadingComponent>
+      </DrizzleProvider>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/drizzleOptions.js
@@ -0,0 +1,7 @@
+import ERC721Token from "./contracts/ERC721Token.json";
+
+const options = {
+  contracts: [ERC721Token],
+};
+
+export default options;
